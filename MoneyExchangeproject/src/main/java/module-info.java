6ce641ed354e27ef50ac98e664454313da3c417b
Tanvirulturjo55 <158068707+Turjo55@users.noreply.github.com>
2024-12-08 14:19:31 +0600

    Delete MoneyExchangeproject directory

--- MoneyExchangeproject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.oop.example.moneyexchangeproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.oop.example.moneyexchangeproject to javafx.fxml;
-    exports com.oop.example.moneyexchangeproject;
-}
